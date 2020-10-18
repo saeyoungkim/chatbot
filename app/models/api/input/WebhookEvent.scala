34cbfdf10ec59e81f9b632c15e282379a5ece3d5
s.kim <s.kim@bizreach.co.jp>
2020-10-18 18:12:33 +0900

    fix

--- app/models/api/input/WebhookEvent.scala
+++ app/models/api/input/WebhookEvent.scala
@@ -1,10 +1,11 @@
 package models.api.input
 
+import models.api.input.event.Event
 import play.api.libs.json.Json
 
 case class WebhookEvent(
   destination: String,
-  events: Seq[WebhookEvent]
+  events: Seq[Event]
 )
 
 object WebhookEvent {

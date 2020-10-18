a055dd425cc194bf4cf48ed692f8aacbcb3a7b95
s.kim <s.kim@bizreach.co.jp>
2020-10-18 18:27:21 +0900

    test

--- app/controllers/WebhookController.scala
+++ app/controllers/WebhookController.scala
@@ -2,7 +2,7 @@ package controllers
 
 import javax.inject.Inject
 import models.api.input.WebhookEvent
-import play.api.libs.json.{JsNumber, Json}
+import play.api.libs.json.{JsError, JsNumber, JsSuccess, Json}
 import play.api.mvc.{AbstractController, ControllerComponents}
 import utils.{LineVerifier, Logger}
 
@@ -16,6 +16,10 @@ class WebhookController @Inject()(
   def webhook() = Action.async{ implicit req =>
     logger.info("message received")
     println(req.body.asJson.map(Json.stringify(_)))
+    req.body.asJson.map(_.validate[WebhookEvent] match {
+      case JsSuccess(v, _) => println(v)
+      case JsError(_) => println(_)
+    })
 
     if(lineVerifier.validateSignature) Future.successful(Ok(Json.obj("status"->JsNumber(OK), "message"->"accepted")))
     else Future.successful(BadRequest(Json.obj("status"->JsNumber(BAD_REQUEST), "message"->"unaccepted")))
--- app/models/api/input/event/EventType.scala
+++ app/models/api/input/event/EventType.scala
@@ -5,8 +5,8 @@ import utils.props.enums.{Enum, EnumsFormattableString}
 sealed abstract class EventType(val code: String) extends Enum[String]
 
 object EventType extends EnumsFormattableString[EventType] {
-  case object Message extends EventType(code = "message")
-  case object Follow extends EventType(code = "follow")
+  case object Message extends EventType("message")
+  case object Follow extends EventType("follow")
 
   override protected val values: Seq[EventType] = Seq(
     Message,

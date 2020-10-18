1d342f06d68dfe2f9dd754ad1c22d20f4cdc7e67
s.kim <s.kim@bizreach.co.jp>
2020-10-18 21:04:24 +0900

    get webhookevent from request

--- app/controllers/HelloController.scala
+++ app/controllers/HelloController.scala
@@ -1,11 +1,12 @@
 package controllers
 
 import javax.inject.Inject
+import play.api.Logger
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
-import utils.Logger
 
-class HelloController @Inject()(cc: ControllerComponents) extends AbstractController(cc)
-  with Logger {
+class HelloController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+  private val logger = Logger(this.getClass)
+
   def hello: Action[AnyContent] = Action { implicit req =>
     throwException()
   }
--- app/controllers/WebhookController.scala
+++ app/controllers/WebhookController.scala
@@ -2,26 +2,23 @@ package controllers
 
 import javax.inject.Inject
 import models.api.input.WebhookEvent
+import play.api.Logger
 import play.api.libs.json.{JsError, JsNumber, JsSuccess, Json}
 import play.api.mvc.{AbstractController, ControllerComponents}
-import utils.{LineVerifier, Logger}
+import utils.LineVerifier
 
 import scala.concurrent.Future
 
 class WebhookController @Inject()(
   cc: ControllerComponents,
   lineVerifier: LineVerifier
-) extends AbstractController(cc)
-  with Logger {
-  def webhook() = Action.async{ implicit req =>
-    logger.info("message received")
-    println(req.body.asJson.map(Json.stringify(_)))
-    req.body.asJson.map(_.validate[WebhookEvent] match {
-      case JsSuccess(v, _) => println(v)
-      case JsError(errors) => println(errors)
-    })
+) extends AbstractController(cc) {
+  private val logger = Logger(this.getClass)
 
-    if(lineVerifier.validateSignature) Future.successful(Ok(Json.obj("status"->JsNumber(OK), "message"->"accepted")))
+  def webhook() = Action.async(parse.json[WebhookEvent]) { implicit req =>
+    logger.info("message received")
+
+    if(lineVerifier.validateLineSignature(req)) Future.successful(Ok(Json.obj("status"->JsNumber(OK), "message"->"accepted")))
     else Future.successful(BadRequest(Json.obj("status"->JsNumber(BAD_REQUEST), "message"->"unaccepted")))
   }
 }

1daca734ac9d6785da2d8c794482d936af88cc95
s.kim <s.kim@bizreach.co.jp>
2020-10-18 18:38:54 +0900

    fix emojits can get nullable value

--- app/models/api/input/event/message/Message.scala
+++ app/models/api/input/event/message/Message.scala
@@ -1,15 +1,14 @@
 package models.api.input.event.message
 
 import models.api.input.event.message.emoji.Emoji
-
-import play.api.libs.json.__
+import play.api.libs.json.{Reads, Writes, __}
 import play.api.libs.functional.syntax._
 
 case class Message(
   id: String,
   messageType: MessageType,
   text: Option[String],
-  emojis: Seq[Emoji]
+  emojis: Option[Seq[Emoji]]
 )
 
 object Message {
@@ -17,6 +16,6 @@ object Message {
     (__ \ "id").format[String] and
       (__ \ "type").format[MessageType] and
       (__ \ "text").formatNullable[String] and
-      (__ \ "emojis").format[Seq[Emoji]]
+      (__ \ "emojis").formatNullable[Seq[Emoji]]
   )(apply _, unlift(unapply _))
 }
